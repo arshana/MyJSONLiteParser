c0ec9d24a9c9e856908466c12680a1d1bbf555e5
arshana <arshanajain@gmail.com>
2017-04-23 16:55:18 -0700

    first commit

---
+++ src/com/google/codeu/codingchallenge/MyJSON.java
@@ -0,0 +1,154 @@
+// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.codeu.codingchallenge;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Set;
+
+//Turning MyJSON into a HashMap-style class
+final class MyJSON extends HashMap implements JSON {
+	@Override
+	public JSON getObject(String name) {
+		// TODO: implement this
+		if(!isEmpty()){
+			//return (JSON) get(name);
+			return getObject(name, this);
+		}
+		return null;
+	}
+	
+	private JSON getObject(String name, MyJSON temp){
+		if(temp.containsKey(name) && (temp.get(name) instanceof JSON)){
+			return (JSON) get(name);
+		}
+		else if(!temp.containsKey(name)){
+			Set<String> allKeys = temp.keySet();
+			for(String key : allKeys){
+				if(temp.get(key) instanceof JSON){
+					JSON check = ((MyJSON) temp.get(key)).getObject(name);
+					if(check != null){
+						return check;
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public JSON setObject(String name, JSON value) {
+		// TODO: implement this
+		if(!setObject(name, value, this)){
+			put(name, value);
+		}
+		return this;
+	}
+	
+	private boolean setObject(String name, JSON value, MyJSON temp){
+		if(temp.containsKey(name)){
+			temp.put(name, value);
+			return true;
+		}
+		Set<String> allKeys = temp.keySet();
+		for(String key : allKeys){
+			if(temp.get(key) instanceof JSON){
+				boolean check = ((MyJSON) temp.get(key)).setObject(name, value, (MyJSON)temp.get(key));
+				if(check){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public String getString(String name) {
+		// TODO: implement this
+		if(!isEmpty()){
+			//return (JSON) get(name);
+			return getString(name, this);
+		}
+		return null;
+	}
+	
+	private String getString(String name, MyJSON temp){
+		if(temp.containsKey(name) && (temp.get(name) instanceof String)){
+			return (String) get(name);
+		}
+		else if(!temp.containsKey(name)){
+			Set<String> allKeys = temp.keySet();
+			for(String key : allKeys){
+				if(temp.get(key) instanceof JSON){
+					String check = ((MyJSON) temp.get(key)).getString(name);
+					if(check != null){
+						return check;
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public JSON setString(String name, String value) {
+		// TODO: implement this
+		if(!setString(name, value, this)){
+			put(name, value);
+		}
+		return this;
+	}
+	
+	private boolean setString(String name, String value, MyJSON temp){
+		if(temp.containsKey(name)){
+			temp.put(name, value);
+			return true;
+		}
+		Set<String> allKeys = temp.keySet();
+		for(String key : allKeys){
+			if(temp.get(key) instanceof JSON){
+				boolean check = ((MyJSON) temp.get(key)).setString(name, value, (MyJSON)temp.get(key));
+				if(check){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	@Override
+	//wants the keys, NOT the values
+	public void getObjects(Collection<String> names) {
+		// TODO: implement this
+		getObjects(names, this);
+	}
+	
+	private void getObjects(Collection<String> names, MyJSON temp){
+		if(!temp.isEmpty()){
+			Set<String> allKeys = temp.keySet();
+			for(String key : allKeys){
+				if(temp.getObject(key) != null){
+					names.add(key);
+					//temp.getObject(key)
+				}
+			}
+		}
+	}
+
+	@Override
+	public void getStrings(Collection<String> names) {
+		// TODO: implement this
+	}
+}
---
+++ src/com/google/codeu/codingchallenge/TestMe.java
@@ -0,0 +1,106 @@
+package com.google.codeu.codingchallenge;
+
+public class TestMe {
+	public static void main(String[] args){
+		MyJSON name = new MyJSON();
+		name.setString("name", "Raahil");
+		//name:Raahil
+		//Should return "Raahil"
+		System.out.println(name.getString("name"));
+		
+		System.out.println(name);
+		System.out.println();
+		
+		MyJSON first = new MyJSON();
+		name.setObject("name", first);
+		//name:{}
+		//Should return "{}"
+		System.out.println(name.getObject("name"));
+		
+		System.out.println(name);
+		System.out.println();
+		
+		first.setString("first", "Reelu");
+		//name:{first:Reelu}
+		//Should return "{first=Reelu}"
+		System.out.println(name.getObject("name"));
+		
+		System.out.println(name);
+		System.out.println();
+		
+		MyJSON sisterFirst = new MyJSON();
+		first.setObject("sisterFirst", sisterFirst);
+		System.out.println(name.get("name"));
+		
+		System.out.println(name);
+		System.out.println();
+		
+		MyJSON second = new MyJSON();
+		second.setString("second", "Sahib");
+		first.setObject("first", second);
+		//Should return "{second=Sahib}"
+		System.out.println(name.getObject("first"));
+		
+		System.out.println(name);
+		System.out.println();
+		
+		MyJSON third = new MyJSON();
+		second.setObject("second", third);
+		third.setString("third", "Jain");
+		//Should return "{third=Jain}"
+		System.out.println(name.getObject("second"));
+		
+		System.out.println(name);
+		System.out.println();
+		
+		//Should return "Jain"
+		System.out.println(name.getString("third"));
+		
+		System.out.println(name);
+		System.out.println();
+		
+		MyJSON mother = new MyJSON();
+		mother.setString("mother", "Anu");
+		name.setObject("first", third);
+		
+		System.out.println(name);
+		System.out.println();
+		
+		name.setObject("third", mother);
+		
+		System.out.println(name);
+		System.out.println();
+		
+		System.out.println(name.getObject("name"));
+		
+		System.out.println(name);
+		System.out.println();
+		
+		System.out.println(second.getObject("second"));
+		
+		System.out.println(second);
+		System.out.println();
+		
+		System.out.println("/////////////////////////////////Test setString()////////////////////////////////////\n");
+		
+		name.setString("mother", "Anu Jain");
+		System.out.println(name);
+		System.out.println();
+		
+		name.setString("sisterFirst", "Arshana");
+		System.out.println(name);
+		System.out.println();
+		
+		name.setObject("first", second);
+		System.out.println(name);
+		System.out.println();
+		
+		name.setString("second", "Sahib");
+		System.out.println(name);
+		System.out.println();
+		
+		name.setObject("second", third);
+		System.out.println(name);
+		System.out.println();
+	}
+}
